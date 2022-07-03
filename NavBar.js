58b11983561fa5972e9b61970d4b3a3122fb1254
Talal-q19 <106393662+Talal-q19@users.noreply.github.com>
2022-07-03 16:09:52 +0400

    Add files via upload
    navBar for app.js react

---
+++ NavBar.js
@@ -0,0 +1,20 @@
+function NavBar(){
+    return(
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+  <a href="./applelogo." class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32"><a href=""></a></svg>
+    <span class="fs-4">iStyle</span>
+  </a>
+
+  <ul class="nav nav-pills">
+    <li class="nav-item"><a href="./https://www.apple.com/ae/" class="nav-link">Home</a></li>
+    <li class="nav-item"><a href="./https://www.apple.com/ae/store" class="nav-link">All Products</a></li>
+    <li class="nav-item"><a href="./https://www.apple.com/ae/shop/accessories/all" class="nav-link">Accessories</a></li>
+    <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+    <li class="nav-item"><a href="./https://support.apple.com/en-ae" class="nav-link">Contact Us</a></li>
+  </ul>
+</header>
+    )
+}
+
+export default NavBar;
