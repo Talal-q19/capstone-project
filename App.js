bb120d89f9d18e2cd49a8d0d90440b7f36f64273
Talal-q19 <106393662+Talal-q19@users.noreply.github.com>
2022-07-03 16:09:11 +0400

    Add files via upload
    app.js for react

---
+++ App.js
@@ -0,0 +1,16 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div >
+      <NavBar></NavBar>
+      <Hero></Hero>
+     <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
