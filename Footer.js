215e3f1b3df34c86d76fac65aedc886603d65426
Talal-q19 <106393662+Talal-q19@users.noreply.github.com>
2022-07-03 16:11:08 +0400

    Add files via upload
    footer for app.js react

---
+++ Footer.js
@@ -0,0 +1,51 @@
+function Footer(){
+    return(
+        <footer class="container py-5">
+  <div class="row">
+    <div class="col-12 col-md">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
+      <small class="d-block mb-3 text-muted">© 2015–2022</small>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Shop and Learn</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/store">Store</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/mac/">Mac</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/ipad/">iPad</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/iphone/">iPhone</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/airtag/">AirTag</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/shop/accessories/all">Accessories</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Services</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/apple-music/">Music</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/apple-fitness-plus/">Fitness</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/apple-podcasts/">Podcast</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/icloud/">iCloud+</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Find Store</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/retail/">Store Locations</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/today/collection/apple-camp/">Today at Store</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/shop/browse/finance/installments">Finance </a></li>
+        <li><a class="link-secondary" href="./https://secure2.store.apple.com/ae/shop/signIn/orders?ssi=1AAABgal4dzcgK3xuH3iH7l4kfhrA0hkl58kwjMFCfToOQ3NadbPBTJAAAAA0aHR0cHM6Ly9zZWN1cmUyLnN0b3JlLmFwcGxlLmNvbS9hZS9zaG9wL29yZGVyL2xpc3R8fAACAXL_DOreV_HYSf3Zl1HxXTkH9heNyoyrT2Ih4rapHgEd">Fiind Your Order</a></li>
+      </ul>
+    </div>
+    <div class="col-6 col-md">
+      <h5>Account</h5>
+      <ul class="list-unstyled text-small">
+        <li><a class="link-secondary" href="./https://appleid.apple.com/">Manage Account </a></li>
+        <li><a class="link-secondary" href="./https://secure2.store.apple.com/ae/shop/signIn/account?ssi=1AAABgal-Qc8g50h3LHBv-WIzVq_30-tPRpKOy13uq4fnqNSVaPOARz4AAAA2aHR0cHM6Ly9zZWN1cmUyLnN0b3JlLmFwcGxlLmNvbS9hZS9zaG9wL2FjY291bnQvaG9tZXx8AAIBnDGu_oObNH3BE6o9ayv2rV8tLykbna7U6GaaBdwKcXg">Store ID</a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/shop/help">Shooping Help </a></li>
+        <li><a class="link-secondary" href="./https://www.apple.com/ae/contact/">Contact Store</a></li>
+      </ul>
+    </div>
+  </div>
+</footer>
+    )
+}
+export default Footer;
